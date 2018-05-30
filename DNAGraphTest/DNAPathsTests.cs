4bef49762c4d23ca7026efcd367f31a450d1aa0d
GorgeousMooseNipple <orang768@gmail.com>
2018-05-30 12:54:55 +0300

    Project Loaded

---
+++ DNAGraphTest/DNAPathsTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using UKPO2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DNAGraphTest
+{
+    [TestClass]
+    public class GraphUnitTest
+    {
+        //Проверяется правильность количества найденных путей
+        [TestMethod]
+        public void FoundPathsCountTest()
+        {
+            String molecule = "АГЦЦГГУААЦЦ";
+            String[] fragmentsArray = { "АГЦЦ", "ЦГГУ", "ГГУАА", "УААЦЦ" };
+            DNAGraph graph = new DNAGraph(molecule, fragmentsArray);
+            List<List<String>> paths = graph.GetPaths();
+
+            Assert.AreEqual(3, paths.Count);
+        }
+
+        //Проверяется ход выполнения при задании пустого графа
+        [TestMethod]
+        public void EmptyGraphTest()
+        {
+            String molecule = "АГЦЦГГУААЦЦ";
+            String[] fragmentsArray = { };
+            DNAGraph graph = new DNAGraph(molecule, fragmentsArray);
+            var verticles = graph.VerticleList;
+
+            Assert.AreEqual(0, verticles.Count);
+        }
+
+        //Одинаковые фрагменты молекулы
+        [TestMethod]
+        public void SameFragmentsTest()
+        {
+            String molecule = "АГЦЦАГЦЦАГЦЦ";
+            String[] fragmentsArray = { "АГЦЦ", "АГЦЦ", "АГЦЦ" };
+            DNAGraph graph = new DNAGraph(molecule, fragmentsArray);
+            var verticles = graph.VerticleList;
+
+            List<String> neighbours = new List<string>() { "АГЦЦ" };
+
+            Assert.AreEqual(verticles[0].GetNeighbours(), neighbours);
+            Assert.AreEqual(verticles[1].GetNeighbours(), neighbours);
+            Assert.AreEqual(verticles[2].GetNeighbours(), new List<String>());
+        }
+
+        // Проверяется корректность найденных путей
+        [TestMethod]
+        public void PathFindingTest()
+        {
+            String molecule = "АГЦЦГГУААЦЦ";
+            String[] fragmentsArray = { "АГЦЦ", "ЦГГУ", "ГГУАА", "УААЦЦ" };
+            DNAGraph graph = new DNAGraph(molecule, fragmentsArray);
+            List<List<String>> paths = graph.GetPaths();
+
+            List<String> path1 = new List<String>() { "АГЦЦ", "ЦГГУ", "ГГУАА", "УААЦЦ" };
+            List<String> path2 = new List<String>() { "АГЦЦ", "ЦГГУ", "УААЦЦ" };
+            List<String> path3 = new List<String>() { "АГЦЦ", "ГГУАА", "УААЦЦ" };
+
+            Assert.IsTrue(path1.SequenceEqual(paths[0]));
+            Assert.IsTrue(path2.SequenceEqual(paths[1]));
+            Assert.IsTrue(path3.SequenceEqual(paths[2]));
+        }
+
+        [TestMethod]
+        public void GraphBuildingTest()
+        {
+            String molecule = "АГЦЦГГУАП";
+            String[] fragmentsArray = { "АГЦЦ", "ЦГГУ", "ЦГГУ", "ГГУЦ", "ГУА" };
+            DNAGraph graph = new DNAGraph(molecule, fragmentsArray);
+            var verticlesList = graph.VerticleList;
+            var verticlesNeighboursFragments = new List<List<String>>() {
+                new List<String>() { "ЦГГУ" },
+                new List<String>() { "ГУА" },
+                new List<String>(),
+                new List<String>()
+               };
+
+            for (var i = 0; i < verticlesList.Count; ++i)
+            {
+                var neighboursFragments = new List<String>();
+                foreach (var neighbour in verticlesList[i].GetNeighbours())
+                {
+                    neighboursFragments.Add(neighbour.Fragment);
+                }
+                Assert.IsTrue(verticlesNeighboursFragments[i].SequenceEqual(neighboursFragments));
+            }
+        }
+    }
+}
---
+++ FuckingInterfaceTests/NiceTestsClass.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.MenuItems;
+using TestStack.White.UIItems.ListBoxItems;
+using TestStack.White.UIItems.TreeItems;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems.WindowStripControls;
+using System.Threading;
+
+using WhiteItems = TestStack.White.UIItems;
+
+namespace FuckingInterfaceTests
+{
+    [TestClass]
+    public class NiceTestsClass
+    {
+        TestStack.White.Application app;
+        Window window;
+
+        Button submitButton, calculateButton, restartButton;
+        TextBox moleculeName, fragmentsNum;
+        ListBox fragmentsList, pathsList;
+        Label moleculeNameLabel;
+        CheckBox testCheckBox;
+
+        [TestInitialize]
+        public void InitApplicationForTests()
+        {
+            app = TestStack.White.Application.Launch("C:\\Users\\Dima\\Documents\\Visual Studio 2017\\Projects\\UKPO2\\Interface\\bin\\Debug\\interface.exe");
+            window = app.GetWindow("MainWindow");
+            submitButton =
+                window.Get<Button>(SearchCriteria.ByAutomationId("SubmitInputButton"));
+            calculateButton =
+                window.Get<Button>(SearchCriteria.ByAutomationId("FindPathsButton"));
+            restartButton =
+                window.Get<Button>(SearchCriteria.ByAutomationId("RestartButton"));
+            moleculeName = 
+                window.Get<TextBox>(SearchCriteria.ByAutomationId("MoleculeInputTextBox"));
+            fragmentsNum =
+                window.Get<TextBox>(SearchCriteria.ByAutomationId("FragNumTextBox"));
+            moleculeNameLabel =
+                window.Get<Label>(SearchCriteria.ByAutomationId("MoleculeNameLabel"));
+            fragmentsList =
+                window.Get<ListBox>(SearchCriteria.ByAutomationId("FragmentsListBox"));
+            pathsList =
+                window.Get<ListBox>(SearchCriteria.ByAutomationId("OutputPathsListBox"));
+            testCheckBox =
+                window.Get<CheckBox>(SearchCriteria.ByAutomationId("TestDataCheckBox"));
+        }
+
+        [TestMethod]
+        public void AllElementsFoundTest()
+        {
+            Assert.IsNotNull(submitButton);
+            Assert.IsNotNull(calculateButton);
+            Assert.IsNotNull(restartButton);
+            Assert.IsNotNull(moleculeName);
+            Assert.IsNotNull(fragmentsNum);
+            Assert.IsNotNull(fragmentsList);
+            Assert.IsNotNull(pathsList);
+            Assert.IsNotNull(moleculeNameLabel);
+        }
+
+        [TestMethod]
+        public void SubmitEmptyInputTest()
+        {
+            submitButton.Click();
+            window.WaitWhileBusy();
+            var messageBox =
+                window.MessageBox("");
+            Assert.IsNotNull(messageBox);
+            var mbLabel = messageBox.Get<TestStack.White.UIItems.Label>(SearchCriteria.Indexed(0));
+            Assert.AreEqual("Поля ввода не заполнены!", mbLabel.Text);
+            messageBox.Close();
+        }
+
+        [TestMethod]
+        public void OutputIsDisabledTest()
+        {
+            Assert.IsFalse(restartButton.Enabled);
+            Assert.IsFalse(calculateButton.Enabled);
+            Assert.IsFalse(fragmentsList.Enabled);
+            Assert.IsFalse(pathsList.Enabled);
+            Assert.IsFalse(moleculeNameLabel.Enabled);
+        }
+
+        [TestMethod]
+        public void OutputEnabledAfterInputTest()
+        {
+            moleculeName.Text = "SomeName";
+            fragmentsNum.Text = "4";
+            submitButton.Click();
+            window.WaitWhileBusy();
+            Assert.IsTrue(restartButton.Enabled);
+            Assert.IsTrue(calculateButton.Enabled);
+            Assert.IsTrue(fragmentsList.Enabled);
+            Assert.IsTrue(pathsList.Enabled);
+            Assert.IsTrue(moleculeNameLabel.Enabled);
+        }
+
+        [TestMethod]
+        public void FragmentsListBoxFillTest()
+        {
+            moleculeName.Text = "SomeFakeValue";
+            fragmentsNum.Text = "11";
+            int fragNumber = int.Parse(fragmentsNum.Text);
+            submitButton.Click();
+            window.WaitWhileBusy();
+            //fragmentsList =
+            //    window.Get<WhiteItems.ListBoxItems.ListBox>(SearchCriteria.ByAutomationId("FragmentsListBox"));
+            //Assert.AreEqual(fragNumber, fragmentsList.Items.Count);
+            
+            Assert.IsTrue(fragmentsList.Items.Count > 0);
+        }
+
+        [TestMethod]
+        public void PathsListFillTest()
+        {
+            testCheckBox.Click();
+            moleculeName.Text = "SomeFakeValue";
+            fragmentsNum.Text = "11";
+            submitButton.Click();
+            window.WaitWhileBusy();
+            calculateButton.Click();
+            window.WaitWhileBusy();
+            Assert.IsTrue(pathsList.Items.Count > 0);
+        }
+
+        [TestMethod]
+        public void RestartButtonTest()
+        {
+            moleculeName.Text = "JustToEnable";
+            fragmentsNum.Text = "2";
+            submitButton.Click();
+            window.WaitWhileBusy();
+            restartButton.Click();
+            window.WaitWhileBusy();
+            Assert.AreEqual(pathsList.Items.Count, 0);
+            Assert.AreEqual(fragmentsList.Items.Count, 0);
+            Assert.AreEqual(moleculeNameLabel.Text, "");
+        }
+
+        [TestCleanup]
+        public void CleanUpAfterTests()
+        {
+            if(!window.IsClosed)
+                window.Close();
+            app.Close();
+        }
+    }
+}
